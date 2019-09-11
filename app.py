44d3e38d91ed0c543f08b3b4da2e8fe2ad37fdd6
Gummy27 <brimir27@outlook.com>
2019-09-11 11:02:01 +0000

    Bætt var við fréttum

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
+import csv
+
+with open('frettir.csv', 'r', newline='', encoding='utf-8') as file:
+	reader = list(csv.reader(file, delimiter=':'))
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return(render_template('default.html'))
+	return render_template('default.html', len=len(reader), list=reader)
 
-@app.route("/kennitala/<kt>")
-def kt(kt):
-	summa = 0
-	for x in kt:
-		summa += int(x)
-
-	return(render_template('kt_summa.html', kt=kt, summa=summa))
-
+@app.route("/frettir/<frett>")
+def frett(frett):
+	article = reader[int(frett)-1]
+	return render_template('Article.html', headline=article[0], greinin=article[1])
